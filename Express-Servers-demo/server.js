076b59ae091085697f085fa032831fb803e85c5c
beccajekogian <rebecca.jekogian23@trinityschoolnyc.org>
2023-01-05 10:40:50 -0500

    changed

--- Express-Servers-demo/server.js
+++ Express-Servers-demo/server.js
@@ -78,14 +78,14 @@ app.get('/post/:postID', function(request, response) {
   // using dynamic routes to specify resource request information
   let postID = request.params.postID;
 
-  if(opponents[opponentName]){
-    opponents[opponentName].win_percent = (opponents[opponentName].win/parseFloat(opponents[opponentName].win+opponents[opponentName].lose+opponents[opponentName].tie) * 100).toFixed(2);
-    if(opponents[opponentName].win_percent=="NaN") opponents[opponentName].win_percent=0;
+  if(posts[postID]){
+    // opponents[opponentName].win_percent = (opponents[opponentName].win/parseFloat(opponents[opponentName].win+opponents[opponentName].lose+opponents[opponentName].tie) * 100).toFixed(2);
+    // if(opponents[opponentName].win_percent=="NaN") opponents[opponentName].win_percent=0;
 
     response.status(200);
     response.setHeader('Content-Type', 'text/html')
     response.render("opponentDetails",{
-      opponent: opponents[opponentName]
+      post: posts[postID]
     });
 
   }else{
@@ -97,29 +97,40 @@ app.get('/post/:postID', function(request, response) {
   }
 });
 
-app.get('/opponentCreate', function(request, response) {
+app.get('/postCreate', function(request, response) {
     response.status(200);
     response.setHeader('Content-Type', 'text/html')
-    response.render("opponentCreate");
+    response.render("postCreate");
 });
 
 //this should be good
 app.post('/postCreate', function(request, response) {
     let postID = request.body.postID; //connects to name="postID" in the ejs form
-    let postPhoto = request.body.postPhoto;
     let postTopic = request.body.postID.topic;
     let postContent = request.body.postID.content;
     let postTitle = request.body.postID.title;
     if(postID&&postTopic&&postContent&&postTitle){
       let posts = JSON.parse(fs.readFileSync('data/posts.json'));
-      let newPost={
+      let newPost = {
         "postID": postID,
         "topic": postTopic,
         "title": postTitle,
         "content": postContent,
-        "photo": postPhoto,
       }
       posts[postID] = newPost;
+
+      let topicsList = posts['topics'];
+      for (let post in posts){
+        //let postTopic = post['topic'].toLowerCase().trim();
+        if (topicsList.hasOwnProperty(postTopic)){
+          //if the topic already exists
+          // topicsList[postTopic][post.postID] = post;
+          topicsList[postTopic] = parseInt(topicsList[postTopic]) + 1; //increase the count of posts under that topic
+        } else{
+          //if the topic doesn't exist yet in topicsList
+          topicsList[postTopic] = 1;
+        }
+
       fs.writeFileSync('data/posts.json', JSON.stringify(posts));
 
       response.status(200);
@@ -145,58 +156,6 @@ app.use("", function(request, response){
     "errorCode":"404"
   });
 });
-
-//
-// app.get('/viewContent', function(request, response) {
-//     let posts = JSON.parse(fs.readFileSync('data/posts.json'));
-//
-//     //accessing URL query string information from the request object
-//     let opponent = request.query.opponent;
-//     let playerThrow = request.query.throw;
-//
-//     if(opponents[opponent]){
-//       let opponentThrowChoices=["Paper", "Rock", "Scissors"];
-//       let results={};
-//
-//       results["playerThrow"]=playerThrow;
-//       results["opponentName"]=opponent;
-//       results["opponentPhoto"]=opponents[opponent].photo;
-//       results["opponentThrow"] = opponentThrowChoices[Math.floor(Math.random() * 3)];
-//
-//       if(results["playerThrow"]===results["opponentThrow"]){
-//         results["outcome"] = "tie";
-//       }else if(results["playerThrow"]==="Paper"){
-//         if(results["opponentThrow"]=="Scissors") results["outcome"] = "lose";
-//         else results["outcome"] = "win";
-//       }else if(results["playerThrow"]==="Scissors"){
-//         if(results["opponentThrow"]=="Rock") results["outcome"] = "lose";
-//         else results["outcome"] = "win";
-//       }else{
-//         if(results["opponentThrow"]=="Paper") results["outcome"] = "lose";
-//         else results["outcome"] = "win";
-//       }
-//
-//       if(results["outcome"]=="lose") opponents[opponent]["win"]++;
-//       else if(results["outcome"]=="win") opponents[opponent]["lose"]++;
-//       else opponents[opponent]["tie"]++;
-//
-//       //update opponents.json to permanently remember results
-//       fs.writeFileSync('data/posts.json', JSON.stringify(posts));
-//
-//       response.status(200);
-//       response.setHeader('Content-Type', 'text/html')
-//       response.render("results", {
-//         data: results
-//       });
-//     }else{
-//       response.status(404);
-//       response.setHeader('Content-Type', 'text/html')
-//       response.render("error", {
-//         "errorCode":"404"
-//       });
-//     }
-// });
-
 
 //..............Start the server...............................//
 const port = process.env.PORT || 3000;
